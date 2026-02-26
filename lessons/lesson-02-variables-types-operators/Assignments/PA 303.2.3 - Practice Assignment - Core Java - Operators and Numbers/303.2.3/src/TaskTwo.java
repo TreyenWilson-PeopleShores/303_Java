468caedc20a19fb38747d01716f521b79a3e82e3
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-26 14:51:41 -0500

    Backup

---
+++ lessons/lesson-02-variables-types-operators/Assignments/PA 303.2.3 - Practice Assignment - Core Java - Operators and Numbers/303.2.3/src/TaskTwo.java
@@ -0,0 +1,24 @@
+public class TaskTwo {
+    public static void main(String[] args) {
+
+        // 1. Code Below
+        int x = 150;
+        System.out.println("Binary X: " + (Integer.toBinaryString(x)));
+        x = x >> 2; // The binary should be have an additional 1 at the end, and the decimal should increase
+        System.out.println("After ByteShift for X: " + (x));
+
+        int a = 225;
+        System.out.println("Binary A: " + (Integer.toBinaryString(a)));
+        a = a >> 2;
+        System.out.println("After ByteShift for A: " + (a));
+        int b = 1555;
+        System.out.println("Binary B: " + (Integer.toBinaryString(b)));
+        b = b >> 2;
+        System.out.println("After ByteShift for B: " + (b));
+        int c = 32456;
+        System.out.println("Binary C: " + (Integer.toBinaryString(c)));
+        c = c >> 2;
+        System.out.println("After ByteShift for C: " + (c));
+
+    }
+}
