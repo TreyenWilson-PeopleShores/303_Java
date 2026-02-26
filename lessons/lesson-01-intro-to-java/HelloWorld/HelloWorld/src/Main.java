aee0f694d1e5be98cd31fa3095fa6c225c44f53d
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-02-26 10:17:44 -0500

    Backup

--- lessons/lesson-01-intro-to-java/HelloWorld/HelloWorld/src/Main.java
+++ lessons/lesson-01-intro-to-java/HelloWorld/HelloWorld/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("My name is W!");
+        double num = 12.3;
+        //new Scanner(Syetem.in)
+        System.out.println(num);
+        int tempNum = (int) num;
+        if(num>=tempNum+.5){
+            num+=1;
+            System.out.println("Rounded up:");
+            System.out.println((int) num);
+        } else if(num<tempNum+.5){
+            System.out.println("Rounded down:");
+            System.out.println(tempNum);
+        }
     }
 }
